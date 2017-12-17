36314b271f07c0064ba2a67cebab488ffff1df41
Aleksandra <PivovarAO@gmail.com>
2017-12-17 23:48:05 +0200

    add js for tabs

---
+++ Homework_8/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('a').on('click', function(e){
+		e.preventDefault();
+		var tabName = $(this).attr('href');
+
+		$('.tab-tab a').removeClass('active');
+		$('.tab-cont li').removeClass('active');
+
+		$(this).addClass('active');
+		$(tabName).addClass('active');
+	});
+});
